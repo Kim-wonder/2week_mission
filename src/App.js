baddef73eeb6a98a103e0b3f32d6f0960aa3b54f
Kim-wonder <glow2myself@gmail.com>
2022-08-04 16:04:43 +0900

    숙련 과제 미완성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+// src/App.js
+
+
+// import React from "react";
+// import { useDispatch } from "react-redux"; // import 해주세요.
+
+// const App = () => {
+//   const dispatch = useDispatch(); // dispatch 생성
+//   return (
+//     <div>
+//       <button>+ 1</button> {/* 버튼을 하나 추가해주세요. */}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import React from 'react';
+import TodoList from './pages/TodoList';
 import './App.css';
+// import { Route, Routes } from './shared/Router';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    // <Routes>
+      <TodoList />
+    //   <Route path="/" elemont={<TodoList/>}/>
+    //   <Route path="/:id" elemont={<Detail/>}/>
+    // </Routes>
+
+  )
 }
 
 export default App;
